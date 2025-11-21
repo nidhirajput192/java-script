a74bc1720fc1966aaaf38e8ecc5edddc2b36cbf6
Nidhi Rajput <rajputnidhi862@gmail.com>
2025-11-21 20:15:03 +0000

    Date & Time

---
+++ 01_basics/07_date_time.js
@@ -0,0 +1,12 @@
+//  DATE
+// date ek object h 
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleString())
+
+//js me month 0 se start hote h ---> 0 -> january
+
+let myCreationDate = new Date(2025 , 0, 3)
+console.log()
+
