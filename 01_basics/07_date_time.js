d33aa202df59e4751ee137a30ecc1b3a840a82b8
Nidhi Rajput <rajputnidhi862@gmail.com>
2025-11-22 17:11:17 +0000

    Date & Time

--- 01_basics/07_date_time.js
+++ 01_basics/07_date_time.js
@@ -1,5 +1,8 @@
 //  DATE
 // date ek object h 
+// date is calculated in mili seconds itself.
+
+
 let myDate = new Date()
 console.log(myDate.toString());
 console.log(myDate.toISOString());
@@ -8,5 +11,5 @@ console.log(myDate.toLocaleString())
 //js me month 0 se start hote h ---> 0 -> january
 
 let myCreationDate = new Date(2025 , 0, 3)
-console.log()
+console.log(myCreationDate.toDateString()); // 0 is taken as jan here
 
