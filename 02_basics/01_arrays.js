b27c9997437bae7205b2289f44284eef751c77d5
Nidhi Rajput <rajputnidhi862@gmail.com>
2025-11-23 16:44:24 +0000

    js-methods

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,11 +1,11 @@
-//arrays
-// always written hi square brackets
-// array object h, contain many element in a single variables(collectipn of multiple elment)
-// resizeable array in js
-// mix of data types or array into array
-// indexing start from 0 index
+/*  -->arrays
+always written hi square brackets
+array object h, contain many element in a single variables(collectipn of multiple elment)
+resizeable array in js
+mix of data types or array into array
+indexing start from 0 index
 
-//copy operation in array give always ->>> shallow copy banata h ->> whose property share a same reference point means jo bhi change hoga wo original array me bhi change hoga 
+copy operation in array give always ->>> shallow copy banata h ->> whose property share a same reference point means jo bhi change hoga wo original array me bhi change hoga */ 
 
 const myArr = [1, 2, 3, 4, 5, 6, 7, "nidhi"]
 const heros =["shaktima", "naagraj"]
@@ -22,8 +22,24 @@ const myArr2= new Array(1,2,3,4,5,6,)
 myArr.push(10)
 console.log(myArr);
 
-// 2- pop()
+ /*  2- pop()
 
-// 3- unshift(element)  -> array ke statring me element add ho jaega
-// 4 - shift()  -> starting elemet remove ho jaata h 
-// 5 - 
+ 3- unshift(element)  -> array ke statring me element add ho jaega
+ 4 - shift()  -> starting element remove ho jaata h 
+
+ questions methods--->
+ includes(element which we have to check whether it is in array or not)-- result will be in true or false
+ indexof()
+join()->>convert array into string square bracket me nhi aayega
+
+// Methiod slice, splice    */
+console.log("A", myArr);
+const myn1 = myArr.slice(1,3)  // -> 3rd index not included , it will print 1st and 2nd indexed element
+
+console.log(myn1);
+console.log("B", myArr);
+
+const myn2 = myArr.splice(1,3)  // -->> original array bhi change ho jata h, splice portion cut ho kar array ko change kar jata h 
+console.log("C", myArr);
+console.log(myn2);
+console.log(myArr);
