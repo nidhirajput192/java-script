25df16f1477a4e604a2ca5808eb580b8b4b4bd75
Nidhi Rajput <rajputnidhi862@gmail.com>
2026-04-14 06:06:35 +0000

    comment added

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,7 +1,7 @@
 "use strict";   //treat all JS code as newer version(newer version of js automatically applicable)
 
-/* alert(3+3)  --->> error , we are using node not browser */
-
+/* alert(3+3)  --->> error , we are using nodejs not browser */
+// Code Readability should be high, clean
 console.log(3+3)
 
 console.log("nnidhi");
@@ -10,23 +10,32 @@ let name= "nidhi"
 let age= 18
 let isLoggedIn= false
 
-//------>>types of data types
+//  ECMAScript standard define karti hai, code kaise likhna h, output input kaise lena h...
+
+
 /*
+
+------>>types of data types
+
+, 
 ======> Primitive datatypes
 
 number--> 2 to power 53
 bigint---> big number, less use able
 string --> "" , ''
 boolean -> true, false
-null  ---> stand alone value(represenattaion of null value [khaali h])
+null  ---> stand alone value(represenattaion of null value [khaali h]), null ko assign kar sakte h
 undefined --> value not define
-symbol---> talk about unique
+symbol---> talk about unique, unique ke liye hmesha symbol ki baat karte h
 object---> talk later
 
  */
 
+
+// typeof ---->> to know thw type of datatype.
+
 console.log(typeof "nidhi");    //<------string
-console.log(typeof null);        // <----- null ka type object
+console.log(typeof null);        // <----- null ka type object.
 console.log(typeof undefined);   // <----- undefined ka type undefined
 
 
