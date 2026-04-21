f72ce8e31b71f03b6f6a34f74c4b3bd6a8256c4b
Nidhi Rajput <rajputnidhi862@gmail.com>
2026-04-21 09:01:28 +0000

    comment added

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,6 +1,7 @@
 /*  objects are decleared in 2 ways
 1--> literal   --> singleton nhi bnata h
 2--> constructor   -> hamesha singleton banta h
+// bs dono me singleton ka hi fark h only as ssuch koi fark nhi h working me
 
 -->> Singleton--> literal ki trha decleAR karenge to singleton nhi
       banta h
@@ -28,7 +29,7 @@ const mySym = Symbol("key1")
 
 
 const jsuser1 = {
-    name: "nidhi",
+    name: "nidhi",  // name apne aap me string h datatype
     "full name" : " nidhi rajput", //  -> how to access?? impossible by dot, then how???, to bracket me double commas se 
     age : 18,
     [mySym]: " mykey1",   // Ye symbol h  --->>> INTERVIEW IMPORTANT
@@ -38,24 +39,29 @@ const jsuser1 = {
 
 }
 
-//acessing methods
-console.log(jsuser1.email);  // ese bhi
-console.log(jsuser1["email"]);   // or ese bhi access kar sakte h 
+//acessing methods of objects...
+console.log(jsuser1.email);  // ese bhi but not correct
+console.log(jsuser1["email"]);   // or ese bhi access kar sakte h, yehi best h 
 console.log(jsuser1["full name"]);
 console.log(jsuser1[mySym]);
 
 
 //to change content
 jsuser1.email = "nidhi@amazon.com"
-Object.freeze(jsuser1)  // ---> value lock kar dete h
+//Object.freeze(jsuser1)  // ---> value lock kar dete h
 jsuser1.email = "nidhi@chatgpt"
-console.log(jsuser1[mySym] );
+//console.log(jsuser1[mySym] );
 
 jsuser1.greeting = function(){
     console.log("hello js user");
     
 }
-console.log(jsuser1.greeting); // undefined
+
+jsuser1.greetingTwo = function(){
+    console.log(`hello JS user ${this.name}`)
+}
+console.log(jsuser1.greeting()); // undefined
+console.log(jsuser1.greetingTwo());
 
 
 
