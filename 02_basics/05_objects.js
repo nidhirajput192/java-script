a55597e27cc77a87ef0ae0dd8c7415912712b43d
Nidhi Rajput <rajputnidhi862@gmail.com>
2026-04-22 13:45:19 +0000

    destructuring of objects

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,44 @@
+/*  DE-STRUCTURING of any name or value  */
+
+const course = {
+    coursename:"js",
+    price : "5000",
+    courseInstructor : "teacher"
+
+}
+
+// 1st way
+console.log(course.courseInstructor);    // teacher
+
+// 2nd way
+console.log(course["courseInstructor"]);  // teacher
+
+
+
+
+/*  API's  format */
+
+
+//  everything in string means key and value
+//  json se API's call hoti h to ese structure me aati h
+
+/*
+{ 
+    "name" : " nidhi",
+    "coursename" : "js",                
+    "price" : " free"
+}
+    */
+
+
+
+
+//---> array  format me bhi milta h, array me objects hote h 
+
+[
+    {},
+    {},
+    {}
+]
+
+
