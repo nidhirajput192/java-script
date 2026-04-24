c2aaad95011708be7f3078374e2379a7c4e26a23
Nidhi Rajput <rajputnidhi862@gmail.com>
2026-04-24 13:45:04 +0000

    iife

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,10 +1,28 @@
-/*   Immediately Invoked Function Expressions (IIFE)     */
+/*  
+ Immediately Invoked Function Expressions(IIFE) : jo function
+immidiately invoked ho jae..(global scope ke polluation se problem
+hoti h bht baar, to global scope ke variables ye,golbal sscope ke 
+pollution ko remove karne ke liye humne iife ka use kiya h)  
 
+Immediately Invoked Function Expressions (IIFEs) are functions 
+in JavaScript that run as soon as they are defined.
+*/
+
+
+//  named iife
 (function chai(){
     // named IIFE
     console.log(`DB CONNECTED`);
 })();    // semicolonn is imppo between two iife
 
-( (name) => {
+
+// without named iife
+( (name) => {          //parameter de diya yaha
     console.log(`DB CONNECTED TWO ${name}`);
-} )('nidhi')
+} )('nidhi')     // ayha arguement de diya...
+
+
+//  Arrow Function Version---> function keyword ki jagha arrow laga denge
+(() => {
+  console.log("Also runs immediately!");
+})();
